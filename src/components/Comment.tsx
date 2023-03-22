8b11408c946f171e92f9d5767ae629b92ad3264e
Diogo Silva <diogo@MacBook-Pro-de-Diogo.local>
2023-03-21 22:35:41 -0300

    Final Projeto 1

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -23,7 +23,10 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/diogosilvasantana.png" alt="" />
+      <Avatar 
+        hasBorder={false} 
+        src="https://github.com/diogosilvasantana.png" 
+        alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
